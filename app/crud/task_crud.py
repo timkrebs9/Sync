f6485b59b640c6cb752da9927e824ac3ff00f4d3
Tim Krebs <timkrebs9@gmail.com>
2024-12-07 11:26:57 +0100

    Refactor task_crud.py by adding spacing for improved readability and consistency in function definitions.

--- app/crud/task_crud.py
+++ app/crud/task_crud.py
@@ -1,6 +1,8 @@
 from sqlalchemy.orm import Session
+
 from app.models.task_model import Task
 from app.schemas.task_schema import TaskCreate
+
 
 def create_task(db: Session, task: TaskCreate):
     db_task = Task(**task.model_dump())
@@ -9,11 +11,14 @@ def create_task(db: Session, task: TaskCreate):
     db.refresh(db_task)
     return db_task
 
+
 def get_tasks(db: Session, skip: int = 0, limit: int = 100):
     return db.query(Task).offset(skip).limit(limit).all()
 
+
 def get_task(db: Session, task_id: int):
     return db.query(Task).filter(Task.id == task_id).first()
+
 
 def update_task(db: Session, task_id: int, task: TaskCreate):
     db_task = db.query(Task).filter(Task.id == task_id).first()
@@ -24,10 +29,11 @@ def update_task(db: Session, task_id: int, task: TaskCreate):
     db.commit()
     return db_task
 
+
 def delete_task(db: Session, task_id: int):
     db_task = db.query(Task).filter(Task.id == task_id).first()
     if db_task is None:
         return None  # or raise HTTPException
     db.delete(db_task)
     db.commit()
-    return db_task 
+    return db_task

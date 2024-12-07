45f8077844465fe8eadfc372946f8a1967013169
Tim Krebs <timkrebs9@gmail.com>
2024-12-07 11:27:08 +0100

    Refactor task_model.py by reorganizing imports for improved readability and consistency.

--- app/models/task_model.py
+++ app/models/task_model.py
@@ -1,6 +1,8 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from sqlalchemy import Boolean, Column, DateTime, Integer, String
 from sqlalchemy.sql import func
+
 from app.core.database import Base
+
 
 class Task(Base):
     __tablename__ = "tasks"

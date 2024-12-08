20f421da094d9031f981ec4f021b45579bea423e
Tim Krebs <timkrebs9@gmail.com>
2024-12-08 15:20:01 +0100

    Enhance configuration settings with new database fields
    - Added new fields for PostgreSQL database configuration: postgres_password, postgres_user, postgres_db, and postgres_port.
    - Set default values for SECRET_KEY, ALGORITHM, and ACCESS_TOKEN_EXPIRE_MINUTES for improved security and usability.
    - Updated the Settings class to accommodate new environment variables for database connectivity.

--- app/core/config.py
+++ app/core/config.py
@@ -3,12 +3,19 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     DATABASE_URL: str
-    SECRET_KEY: str
-    ALGORITHM: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    SECRET_KEY: str = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     ENVIRONMENT: str = "development"
     API_V1_PREFIX: str = "/api/v1"
     API_V2_PREFIX: str = "/api/v2"
+    
+    # Add these new fields
+    postgres_password: str
+    postgres_user: str
+    postgres_db: str
+    postgres_port: str
+    ci: str
 
     class Config:
         env_file = ".env"
